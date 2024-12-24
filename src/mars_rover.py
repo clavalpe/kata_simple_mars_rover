bbc19209589ea9fe7779b9d7c9ed16ab13152626
Clara <clavalpe@upv.es>
2024-12-24 13:22:32 +0100

    green - test_it_rotates_to_the_rigth_when_receives_command_R

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -5,8 +5,12 @@ class MarsRover:
         self._direction: str = "N"
 
     def execute(self, command: str) -> str:
-        if len(command) > 0: 
-            for i in range(0, len(command)):
-                self._y_position += 1
+        if command == "R":
+            self._direction: str = "E"
+
+        if command == "M" or command == "MM":
+            if len(command) > 0: 
+                for i in range(0, len(command)):
+                    self._y_position += 1
 
         return f"{self._x_position}:{self._y_position}:{self._direction}"
