cbdc32d0e18285c6e15feec77c27aed0eb2b3bce
Clara <clavalpe@upv.es>
2024-12-24 12:45:42 +0100

    refactor - add MarsRover constructor

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,9 +1,14 @@
 class MarsRover:
+    def __init__(self):
+        self._x_position: int = 0
+        self._y_position: int = 0
+        self._direction: str = "N"
+
     def execute(self, command: str) -> str:
-        if command == 'M':
-            return '0:1:N'
-        
-        if command == 'MM':
-            return '0:2:N'
-        
-        return '0:0:N'
+        if command == "M":
+            return "0:1:N"
+
+        if command == "MM":
+            return "0:2:N"
+
+        return f"{self._x_position}:{self._y_position}:{self._direction}"
--- src/test/test_mars_rover.py
+++ src/test/test_mars_rover.py
@@ -3,16 +3,16 @@ from mars_rover import MarsRover
 
 class TestMarsRover:
     def test_it_starts_in_position_0_0_N(self):
-        rover_position = MarsRover().execute('')
+        rover_position = MarsRover().execute("")
 
-        assert rover_position == '0:0:N'
+        assert rover_position == "0:0:N"
 
     def test_it_moves_one_position_when_receives_command_M(self):
-        rover_position = MarsRover().execute('M')
+        rover_position = MarsRover().execute("M")
 
-        assert rover_position == '0:1:N'
+        assert rover_position == "0:1:N"
 
     def test_it_moves_two_positions_when_receives_command_MM(self):
-        rover_position = MarsRover().execute('MM')
+        rover_position = MarsRover().execute("MM")
 
-        assert rover_position == '0:2:N'
+        assert rover_position == "0:2:N"
